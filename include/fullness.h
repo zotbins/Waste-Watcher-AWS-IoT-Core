d8db3410abe95502b7b457a254dffc125d07a2a0
Patrick Nguyen <41417417+patrickanguyen@users.noreply.github.com>
2021-10-23 21:18:39 -0700

    Addressed code standard violations & removed unneccessary header files (#5)

--- include/fullness.h
+++ include/fullness.h
@@ -10,11 +10,6 @@
 #define FULLNESS_H
 
 #include <stdbool.h>
-#include <driver/gpio.h>
-
-// GPIO Pins for Ultrasonic Sensor
-#define US_ECHO_PIN 12
-#define US_TRIGGER_PIN 13
 
 bool fullness_init(void);
 bool fullness_measure(float *p_distance);
