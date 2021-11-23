558d87e054d054733dfa4fac1b0cd9e96c5b893f
Derek Skinner <60554721+dskin344@users.noreply.github.com>
2021-11-22 20:42:10 -0800

    Flash intializer and toggle (#19)

--- src/main.c
+++ src/main.c
@@ -9,21 +9,37 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <esp_log.h>
+#include "photo.h"
 #include "fullness.h"
 
 void app_main()
 {
-    // initialize fullness sensor for use
     uint32_t fullness;
-    if (!fullness_init()) 
+    bool b_on = true;
+
+    if (!flash_init()) 
     {
-        ESP_LOGE("WasteWatcher","Failed to init fullness sensor");
+        ESP_LOGE("WasteWatcher", "Failed to init flash LED");
+        while (1) 
+        {
+        }
+    }
+    
+    if (!fullness_init())
+    {
+        ESP_LOGE("WasteWatcher", "Failed to init fullness sensor");
+        while (1) 
+        {
+        }
     }
 
     while (1)
     {
         fullness_measure(&fullness);
         ESP_LOGI("WasteWatcher", "Measured Distance: %d", fullness);
+        flash_toggle(b_on);
+        b_on = !b_on;
+
         vTaskDelay(1000 / portTICK_PERIOD_MS); // delays by 1 second
     }
 }
--- src/photo.c
+++ src/photo.c
@@ -5,6 +5,9 @@
  * @brief Functions relating to taking photos of the waste bin
  *
  */
+
+#include <esp_log.h>
+#include <driver/gpio.h>
 #include "photo.h"
 
 /**
@@ -15,19 +18,28 @@
 bool
 flash_init (void)
 {
-    return false;
+    gpio_config_t flash_config =
+    {
+        .intr_type = GPIO_PIN_INTR_DISABLE,
+        .mode = GPIO_MODE_OUTPUT,
+        .pin_bit_mask = (1ULL << FLASH_PIN),
+        .pull_down_en = 0,
+        .pull_up_en = 0
+    };
+
+    return gpio_config(&flash_config) == ESP_OK;
 }
 
 /**
  * @brief Toggles the camera flash
  *
- * @param b_status True/False if the flash should be on/off
+ * @param b_on True/False if the flash should be on/off
  * @return True/False depending if the flash successfully changed state
  */
 bool
 flash_toggle (bool b_on)
 {
-    return false;
+    return gpio_set_level(FLASH_PIN, b_on) == ESP_OK;
 }
 
 /**
